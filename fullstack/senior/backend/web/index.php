2c4a091aa94adbaeb03103776dd14210449cc3a2
Jean-Charles Duhail <ashurai@gmail.com>
2018-04-30 22:09:56 +0200

    index

--- fullstack/senior/backend/web/index.php
+++ fullstack/senior/backend/web/index.php
@@ -1,12 +1,9 @@
 <?php
-
 declare(strict_types=1);
-
 define('ROOT_PATH', realpath('.'));
-
 $filename = __DIR__.preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
 if (php_sapi_name() === 'cli-server' && is_file($filename)) {
     return false;
 }
 
-require_once 'src/Client/Webapp/app.php';
+require_once 'src/app.php';
