476a46010f1f489305ff574384f26f9af1574d2f
Jean-Charles Duhail <ashurai@gmail.com>
2018-05-01 16:24:39 +0200

    last

--- fullstack/senior/backend/tests/Client/Webapp/Tests/AppTest.php
+++ fullstack/senior/backend/tests/Client/Webapp/Tests/AppTest.php
@@ -9,7 +9,7 @@ class AppTest extends WebTestCase
 {
     public function createApplication()
     {
-        return require ROOT_PATH . '/src/Client/Webapp/app.php';
+        return require ROOT_PATH . '/src/app.php';
     }
 
     public function testApiRoot()
@@ -21,4 +21,60 @@ class AppTest extends WebTestCase
         $this->assertTrue($response->isOk());
         $this->assertEquals('Status OK', $response->getContent());
     }
+    
+    public function testSurvey1(){
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/api/v1/surveys/qcm/XX1');
+        $response = $client->getResponse();
+        
+        $this->assertTrue($response->isOk());
+        $this->assertEquals('Response', $response->getContent());
+    }
+    
+    public function testSurvey2(){
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/api/v1/surveys/qcm/XX2');
+        $response = $client->getResponse();
+        
+        $this->assertTrue($response->isOk());
+        $this->assertEquals('Response', $response->getContent());
+    }
+
+    public function testSurvey3(){
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/api/v1/surveys/qcm/XX3');
+        $response = $client->getResponse();
+        
+        $this->assertTrue($response->isOk());
+        $this->assertEquals('Response', $response->getContent());
+    }
+    
+    public function testSurvey4(){
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/api/v1/numeric/XX1');
+        $response = $client->getResponse();
+        
+        $this->assertTrue($response->isOk());
+        $this->assertEquals('Response', $response->getContent());
+    }
+    
+    public function testSurvey5(){
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/api/v1/surveys/numeric/XX2');
+        $response = $client->getResponse();
+        
+        $this->assertTrue($response->isOk());
+        $this->assertEquals('Response', $response->getContent());
+    }
+    
+    public function testSurvey6(){
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/api/v1/surveys/numeric/XX3');
+        $response = $client->getResponse();
+        
+        $this->assertTrue($response->isOk());
+        $this->assertEquals('Response', $response->getContent());
+    }
+    
+    
 }
